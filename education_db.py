a57be9cb2e29d14aed05722242d7f550493b9115
basketking <nemethhaz@gmail.com>
2022-02-17 13:21:34 +0100

    small mod on image

--- education_db.py
+++ education_db.py
@@ -4,9 +4,9 @@ from PIL import Image
 
 
 
-
-st.header('Education Database - Numberssss')
-st.image("https://www.sportaccord.sport/wsbs-2022/wp-content/uploads/sites/2/2020/11/Fiteq1800x1200-e1605615045111-1024x683.png", width = None )
+header1, image1 = st.columns(2)
+header1.header('Education Database - Numberssss')
+image1.image("https://www.sportaccord.sport/wsbs-2022/wp-content/uploads/sites/2/2020/11/Fiteq1800x1200-e1605615045111-1024x683.png", width = None )
 st.subheader('SZLAMB')
 
 ## Load Dataframe

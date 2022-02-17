57419abfdacabfa798161dfa85373221c20ab19f
basketking <nemethhaz@gmail.com>
2022-02-17 16:39:53 +0100

    linkupdate

--- education_db.py
+++ education_db.py
@@ -12,8 +12,8 @@ st.subheader('Filters')
 
 ## Load Dataframe
 
-df_ref = pd.read_excel("https://github.com/basketking/FITEQ-Education-Database/blob/main/Education_database.xlsx?raw=true", sheet_name = 'Referee database')
-df_coach = pd.read_excel("https://github.com/basketking/FITEQ-Education-Database/blob/main/Education_database.xlsx?raw=true", sheet_name = 'Coach database')
+df_ref = pd.read_excel("https://github.com/basketking/FITEQ-Education-Database/blob/main/Education%20database.xlsx?raw=true", sheet_name = 'Referee database')
+df_coach = pd.read_excel("https://github.com/basketking/FITEQ-Education-Database/blob/main/Education%20database.xlsx?raw=true", sheet_name = 'Coach database')
 
 ## Create only relevant information in each table --> Continent, Nationality, Total Ref, Female Ref, Male Ref      - 
 #                                                     Continent, Nationality, Total Coach, Female Coach, Male Coach
